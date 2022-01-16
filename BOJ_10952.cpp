81c05ece8811f9c97cdcb6eb5fa259a492103e7b
Ugyeong <66774144+Ugyeong@users.noreply.github.com>
2022-01-17 00:17:57 +0900

    BOJ_10952 A + B -5

---
+++ BOJ_10952.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+	int A, B;
+	while (1) {
+		cin >> A >> B;
+		if (A == 0 && B == 0) {
+			break;
+		}
+		cout << A + B<<"\n";
+	}
+
+	return 0;
+}
